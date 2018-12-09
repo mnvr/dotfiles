ce88a5408e0bd9959c428fb97121c9e4c22072c5
Manav Rathi <mnvrth@gmail.com>
2018-12-09 19:59:47 +0200

    Bells and whistles
    Also need to turn off bells Terminal > Preferences > Advanced.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,11 +1,15 @@
 (setq make-backup-files nil)
 (global-font-lock-mode 0)
 (menu-bar-mode 0)
+(setq ring-bell-function 'ignore)
 
 (setq-default require-final-newline t)
 
 (setq-default indent-tabs-mode nil)
 (setq-default c-basic-offset 4)
+(setq-default tab-width 4)
+(setq-default tab-stop-list (number-sequence 4 200 4))
+(setq-default indent-line-function 'tab-to-tab-stop)
 
 (defun pbcopy ()
     (interactive)
