c4f785070f37e035ad9b000a8e529a42473a20c5
Manav Rathi <mnvrth@gmail.com>
2019-01-20 12:45:42 +0200

    indent-relative-4less
    Based on https://emacs.stackexchange.com/questions/3809/indentation-of-newlines

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -11,6 +11,15 @@
 (setq-default tab-stop-list (number-sequence 4 200 4))
 (setq-default indent-line-function 'tab-to-tab-stop)
 
+(defun indent-relative-4less ()
+  "Newline and indent 4 spaces less than previous line."
+  (interactive)
+  (let ((indent (save-excursion (back-to-indentation) (current-column))))
+    (newline)
+    (insert (make-string (- indent 4) ?\s))))
+
+(global-set-key (kbd "C-RET") #'indent-relative-4less)
+
 (defun pbcopy ()
     (interactive)
     (shell-command-on-region (mark) (point) "pbcopy"))
