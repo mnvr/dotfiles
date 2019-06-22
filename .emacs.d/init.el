8dd1bfab304578a5658be03bc304cfbaf1681390
Manav Rathi <mnvrth@gmail.com>
2019-06-22 10:56:39 +0300

    MELPA and stuff

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,12 @@
+(when (>= emacs-major-version 24)
+  ;; Added by Package.el.  This must come before configurations of
+  ;; installed packages.  Don't delete this line.  If you don't want it,
+  ;; just comment it out by adding a semicolon to the start of the line.
+  ;; You may delete these explanatory comments.
+  (package-initialize)
+  (add-to-list 'package-archives
+               '("melpa-stable" . "https://stable.melpa.org/packages/")))
+
 (setq make-backup-files nil)
 (global-font-lock-mode 0)
 (menu-bar-mode 0)
@@ -22,9 +31,41 @@ indent one level less."
 
 (global-set-key (kbd "C-j") #'indent-relative-4less)
 
+(defun mn-close-bracket-and-indent ()
+  "Reduce indentation and insert a closing bracket."
+  (interactive)
+  (let ((indent (save-excursion (back-to-indentation) (current-column))))
+    (newline)
+    (insert (make-string (- indent 4) ?\s))
+    (insert "}")))
+
+(global-set-key (kbd "C-c }") #'mn-close-bracket-and-indent)
+
+(defun mn-add-braces ()
+  "Fancy insert {} and indent."
+  (interactive)
+  (let ((indent (save-excursion (back-to-indentation) (current-column))))
+    (insert "{")
+    (newline)
+    (insert (make-string (+ indent 4) ?\s))
+    (newline)
+    (insert (make-string indent ?\s))
+    (insert "}")
+    (move-end-of-line 0)))
+
+(global-set-key (kbd "C-c {") #'mn-add-braces)
+
+(defun mn-eval-swift-buffer ()
+  (interactive)
+  (save-buffer)
+  (shell-command (concat "swift " (buffer-file-name (current-buffer)))))
+
+(global-set-key (kbd "C-c v") #'mn-eval-swift-buffer)
+
 (defun pbcopy ()
     (interactive)
     (shell-command-on-region (mark) (point) "pbcopy"))
+
 (global-set-key (kbd "C-M-v") 'pbcopy)
 
 (custom-set-variables
