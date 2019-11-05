cb593d65ad07701d1356a2ddd91e5f11b6ca0d68
Manav Rathi <mnvrth@gmail.com>
2019-11-05 21:36:11 +0200

    Go mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -32,11 +32,14 @@
 ;; messages in Fundamental mode (the default), so switch to text mode.
 (add-to-list 'auto-mode-alist '("COMMIT_EDITMSG" . text-mode))
 
+(add-hook 'before-save-hook #'gofmt-before-save)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (go-mode)))
  '(sort-fold-case t t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
