8563807caf47be45b43f1f8242175714cae20dfd
Manav Rathi <mnvrth@gmail.com>
2019-11-14 22:22:19 +0200

    Multiple cursors

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -52,12 +52,21 @@
 ;; messages in Fundamental mode (the default), so switch to text mode.
 (add-to-list 'auto-mode-alist '("COMMIT_EDITMSG" . text-mode))
 
+;; M-x package-install multiple-cursors
+;; https://github.com/magnars/multiple-cursors.el
+(require 'multiple-cursors)
+
+(global-set-key (kbd "C-c C-a") 'mc/edit-lines)
+(global-set-key (kbd "C-c C-n") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-c C-p") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-m") 'mc/mark-all-like-this)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (go-mode)))
+ '(package-selected-packages (quote (multiple-cursors go-mode)))
  '(sort-fold-case t t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
