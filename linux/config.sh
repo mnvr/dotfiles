0ebcb1b1be1418d711795db6c24babbdd80a93f0
Manav Rathi <manav@mnvr.in>
2025-06-15 19:01:35 +0530

    term

--- linux/config.sh
+++ linux/config.sh
@@ -4,8 +4,10 @@ set -o errexit
 set -o nounset
 set -o xtrace
 
-xfconf-query -c keyboard-layout -p /Default/XkbDisable -s false
-xfconf-query -c keyboard-layout -p /Default/XkbLayout  -s us
-xfconf-query -c keyboard-layout -p /Default/XkbVariant -s dvorak
+# Keyboard layout: Dvorak
+xfconf-query -c keyboard-layout -np /Default/XkbDisable -t bool -s false
+xfconf-query -c keyboard-layout -np /Default/XkbLayout  -t string -s us
+xfconf-query -c keyboard-layout -np /Default/XkbVariant -t string -s dvorak
 
-
+# Terminal: "Disable all menu access keys (such as Alt + f)"
+xfconf-query -c xfce4-terminal -np /shortcuts-no-mnemonics -t bool -s true
