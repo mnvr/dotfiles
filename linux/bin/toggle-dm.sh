043fbe8fa7227dd7ae69b97718257fa2c867aec1
Manav Rathi <manav@mnvr.in>
2025-06-29 09:31:35 +0530

    Workaround 2

--- linux/bin/toggle-dm.sh
+++ linux/bin/toggle-dm.sh
@@ -2,21 +2,13 @@
 
 set -o errexit
 set -o nounset
-set -o xtrace
 
+# This script toggles between the light and dark variants of the default theme.
+#
 # Workaround for Xfce 4.20 always displaying the dark variant of Advaita theme
-# (not sure where the issue is, this just copies the upstream theme and removes
-# gtk-dark.css from it).
-if ! test -d ~/.local/share/themes/adw-gtk3-mod
-then
-    mkdir -p ~/.local/share/themes
-    cp -r /usr/share/themes/adw-gtk3 ~/.local/share/themes/adw-gtk3-mod
-    cd ~/.local/share/themes/adw-gtk3-mod
-    sed -i 's/adw-gtk3/adw-gtk3-mod/' index.theme
-    rm gtk-3.0/gtk-dark.css
-    cd -
-fi
+# (not sure where the issue is, but setting the theme name to "adw-gtk" instead
+# of the correct value "adw-gtk3" gets the light variant to show up).
 
-theme="adw-gtk-dark"
-xfconf-query -c xsettings -p /Net/ThemeName | grep -q dark && theme="adw-gtk-mod"
+theme="adw-gtk3-dark"
+xfconf-query -c xsettings -p /Net/ThemeName | grep -q dark && theme="adw-gtk"
 xfconf-query -c xsettings -p /Net/ThemeName -t string -ns $theme
