134cc508ca113f8f1fc457841455508426b28c7f
Manav Rathi <manav@mnvr.in>
2025-06-29 16:01:22 +0530

    Move to script

--- linux/README.md
+++ linux/README.md
@@ -4,7 +4,6 @@ Last used with Alpine Linux.
 git clone https://github.com/mnvr/dotfiles
 cd dotfiles/linux
 sh setup.sh
-sh config.sh
 ```
 
 Documentation accompanying this configuration:
@@ -18,11 +17,3 @@ Ensure signing keys.
 doas apk add openssh-keygen && ls ~/.ssh/id_ed*
 ```
 
-Font.
-
-```sh
-wget FiraCode-latest.zip
-unzip FiraCode*.zip
-mkdir ~/.local/share/fonts/FiraCode
-mv ttf ~/.local/share/fonts/FiraCode
-```
--- linux/setup.sh
+++ linux/setup.sh
@@ -4,16 +4,39 @@ set -o errexit
 set -o nounset
 set -o xtrace
 
+# Add packages ---
+
 doas apk add docs mandoc-apropos
 doas apk add git zsh file coreutils less curl jq
 doas apk add adwaita-xfce-icon-theme adw-gtk3
 
+# Link config ----
+
 d=`pwd`
 link () {
     cd ~/`dirname $1`
-    ln -s "$d/$1" || readlink "$1"
+    ln -s "$d/$1" || readlink `basename $1`
 }
 
 link .gitconfig
 link .zshrc
-mkdir ~/.emacs.d && link .emacs.d/init.el
+mkdir -p ~/.emacs.d && link .emacs.d/init.el
+
+cd "$d"
+
+# Install Fonts ---
+
+if ! test -d ~/.local/share/fonts/FiraCode
+then
+    mkdir /tmp/fira && cd /tmp/fira
+    wget https://github.com/tonsky/FiraCode/releases/download/6.2/Fira_Code_v6.2.zip
+    unzip Fira*.zip
+    mkdir -p ~/.local/share/fonts/FiraCode
+    mv ttf ~/.local/share/fonts/FiraCode
+    cd -
+    rm -rf /tmp/fira
+fi
+
+# Modify Xfce and friends ---
+
+sh config.sh
