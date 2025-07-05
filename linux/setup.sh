ebdd7a0b2a03be06fd1ef6e481623c77b65c68a7
Manav Rathi <manav@mnvr.in>
2025-07-05 19:44:49 +0530

    grub

--- linux/setup.sh
+++ linux/setup.sh
@@ -4,13 +4,13 @@ set -o errexit
 set -o nounset
 set -o xtrace
 
-# Add packages ---
+# Add packages
 
 doas apk add docs mandoc-apropos
 doas apk add git zsh file coreutils less curl jq
 doas apk add adwaita-xfce-icon-theme adw-gtk3
 
-# Link config ----
+# Link config
 
 d=`pwd`
 link () {
@@ -24,7 +24,7 @@ mkdir -p ~/.emacs.d && link .emacs.d/init.el
 
 cd "$d"
 
-# Install Fonts ---
+# Install fonts
 
 if ! test -d ~/.local/share/fonts/FiraCode
 then
@@ -37,17 +37,26 @@ then
     rm -rf /tmp/fira
 fi
 
-# Autologin
-
+# Enable autologin
+#
+# Requires coreutils for 'install'
 test -f /etc/lightdm/lightdm.conf.d/50-autologin.conf || \
 echo | doas install -D -m 644 /dev/stdin /etc/lightdm/lightdm.conf.d/50-autologin.conf <<EOF
 [Seat:*]
 autologin-user=$(whoami)
 EOF
 
-# Modify Xfce and friends ---
+# Remove bootmenu wait
+if ! grep 'GRUB_TIMEOUT=0' /etc/default/grub
+then
+    doas sed -i -E 's/GRUB_TIMEOUT=\d+/GRUB_TIMEOUT=0/' /etc/default/grub
+    doas update-grub
+fi
 
+# Modify Xfce and friends
 sh config.sh
 
-# A symlink doesn't work, copy the file.
+# Modify the panel
+#
+# A symlink doesn't work since we git-ted it, copy the file.
 cp share/xfce4-panel.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml
